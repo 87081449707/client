d72292b78a4170d88ab191a77af49b27f7b2d7a8
87081449707 <87081449707@mail.ru>
2020-07-29 15:18:48 +0600

    1

--- main.js
+++ main.js
@@ -1,16 +1,43 @@
 console.log('Hello World!');
 
-console.log('ok')
-
 //peer js
 var peer = new Peer()
 
 peer.on('open', function(id) {
   alert('id')
   console.log('id: ' + id)
-});
+})
 
 peer.on('error', function(error) {
   alert(error)
-  console.log('error: ' + error)
-});
+  console.log('error peer js: ' + error)
+})
+
+// geolocation
+var geolocation = new Array()
+
+geolocation['x'] = 0
+geolocation['y'] = 0
+
+navigator.geolocation.getCurrentPosition(geolocation_position, geolocation_error);
+
+function geolocation_position (position) {
+	geolocation['x'] = position.coords.latitude.toFixed(7) * 10000000;
+	geolocation['y'] = position.coords.longitude.toFixed(7) * 10000000;
+}
+
+function geolocation_error (error) {
+	alert('error geolocation: ' + error.message);
+}
+
+// touchpad
+/*
+document.body.onmousedown = function(event) {
+  alert('ok')
+  //alert(geolocation)
+}
+*/
+
+document.addEventListener('touchstart', function(event) {
+  alert('ok')
+}, false)
