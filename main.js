12e9b172e217ba5009484137443e13d8a230e91f
87081449707 <87081449707@mail.ru>
2020-08-15 14:58:50 +0600

    1

--- main.js
+++ main.js
@@ -1,43 +1,58 @@
 //console.log('Hello world');
 
-//peer js
-var peer = new Peer()
+// XMLHttpRequest
+var net = new XMLHttpRequest()
 
-peer.on('open', function(id) {
-  //alert('peer js id: ' + id)
-})
-
-peer.on('error', function(error) {
-  //alert('peer js error: ' + error)
-})
-
-var conn
-
-function peerIdConnect() {
-  document.getElementById('log').innerHTML = 'connect button'
-  
-  conn = peer.connect(document.getElementById('peer_id').value)
-  
-  conn.on('open', function() {
-    document.getElementById('log').innerHTML = 'open'
-    //alert('open')
-    conn.send('hi!')
-  })
-  
-  conn.on('error', function(error) {
-    //alert('peer js conn error: ' + error)
-  })
+net.onload = function() {
+  connectServer()
 }
 
+//peerJs
+var client = new Peer()
+var server = []
+var connect = []
 
+client.on('open', function(id) {
+  document.getElementById('log').innerHTML += 'client id: ' + id
+})
+
+client.on('error', function(error) {
+  document.getElementById('log').innerHTML += 'client error: ' + error
+})
+
+net.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
+net.send()
+
+function connectServer() {
+  server.id = JSON.parse(net.responseText).result[JSON.parse(net.responseText).result.length - 1].message.text
+  document.getElementById('log').innerHTML += 'server id: ' + server.id
+  connect = client.connect(server.id)
+}
+
+connect.on('open', function() {
+  document.getElementById('log').innerHTML = 'open'
+})
+
+connect.on('error', function(error) {
+  document.getElementById('log').innerHTML = 'connect error: ' + error
+})
+
+function peerIdConnect() {
+  document.getElementById('log').innerHTML += 'connect button'
+  if (server.lenght == 0) {
+    connect = client.connect(document.getElementById('peer_id').value)
+  }
+  
+  connect.send('hi')
+}
 
 // geolocation
+navigator.geolocation.ge==tCurrentPosition(geolocation_position, geolocation_error)
+
 var geolocation = []
 
-geolocation['x'] = undefined
-geolocation['y'] = undefined
-
-navigator.geolocation.getCurrentPosition(geolocation_position, geolocation_error);
+geolocation['x'] =[]
+geolocation['y'] = []
 
 function geolocation_position (position) {
 	geolocation['x'] = position.coords.latitude//.toFixed(7) * 10000000
@@ -59,4 +74,3 @@ var party = []
 party['other'] = []
 
 party['my'] = []
-
