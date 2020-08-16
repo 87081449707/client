ca0a13e5a9c6fce6453bd5c608070fd1b32a387a
87081449707 <87081449707@mail.ru>
2020-08-16 14:29:02 +0600

    1

--- main.js
+++ main.js
@@ -4,63 +4,61 @@
 var net = new XMLHttpRequest()
 
 net.onload = function() {
-  connectServer()
+  serverId()
 }
 
 //peerJs
 var client = new Peer()
 var server = []
+server.id
 var connect = []
 
 client.on('open', function(id) {
-  document.getElementById('log').innerHTML += 'client id: ' + id
+  //document.getElementById('log').innerHTML += 'client id: ' + id
 })
 
 client.on('error', function(error) {
-  document.getElementById('log').innerHTML += 'client error: ' + error
+  //document.getElementById('log').innerHTML += 'client error: ' + error
 })
 
 net.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
 net.send()
 
-function connectServer() {
+function serverId() {
   server.id = JSON.parse(net.responseText).result[JSON.parse(net.responseText).result.length - 1].message.text
-  document.getElementById('log').innerHTML += 'server id: ' + server.id
+  //document.getElementById('log').innerHTML += 'server id: ' + server.id
+}
+
+var connectServer = setInterval(function() {
   connect = client.connect(server.id)
-}
 
-connect.on('open', function() {
-  document.getElementById('log').innerHTML = 'open'
-})
+  connect.on('open', function() {
+    clearInterval(connectServer)
+    connect.send('hi')
+    //document.getElementById('log').innerHTML = 'open'
+  })
 
-connect.on('error', function(error) {
-  document.getElementById('log').innerHTML = 'connect error: ' + error
-})
-
-function peerIdConnect() {
-  document.getElementById('log').innerHTML += 'connect button'
-  if (server.lenght == 0) {
-    connect = client.connect(document.getElementById('peer_id').value)
-  }
-  
-  connect.send('hi')
-}
+  connect.on('error', function(error) {
+    clearInterval(connectServer)
+    //document.getElementById('log').innerHTML = 'connect error: ' + error
+  })
+}, 1000)
 
 // geolocation
-navigator.geolocation.ge==tCurrentPosition(geolocation_position, geolocation_error)
+navigator.geolocation.getCurrentPosition(geolocation_position, geolocation_error)
 
 var geolocation = []
 
-geolocation['x'] =[]
-geolocation['y'] = []
+geolocation['x'] = undefined
+geolocation['y'] = undefined
 
-function geolocation_position (position) {
-	geolocation['x'] = position.coords.latitude//.toFixed(7) * 10000000
-	geolocation['y'] = position.coords.longitude//.toFixed(7) * 10000000
+function geolocation_position(position) {
+  geolocation['x'] = position.coords.latitude //.toFixed(7) * 10000000
+  geolocation['y'] = position.coords.longitude //.toFixed(7) * 10000000
 }
 
-function geolocation_error (error) {
-	//alert('geolocation error: ' + error.message)
+function geolocation_error(error) {
+  //alert('geolocation error: ' + error.message)
 }
 
 // touchpad
@@ -69,8 +67,22 @@ document.addEventListener('touchstart', function(event) {
 }, false)
 
 // party
+function partyOther () {
+  alert('other')
+}
+
+function partyMy() {
+  alert('my')
+}
+
+
 var party = []
 
 party['other'] = []
+
+
+
+
+
 
 party['my'] = []
