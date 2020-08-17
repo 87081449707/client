011104aa1a6a284a61b65aff2a19e284e11e3f34
87081449707 <87081449707@mail.ru>
2020-08-17 06:58:31 +0300

    1

--- main.js
+++ main.js
@@ -1,51 +1,5 @@
 //console.log('Hello world');
 
-// XMLHttpRequest
-var net = new XMLHttpRequest()
-
-net.onload = function() {
-  peerServerId()
-}
-
-//peerJs
-var peer = []
-peer.client = new Peer()
-peer.server = []
-peer.server.id = []
-peer.connect = []
-peer.connectServer = []
-
-peer.client.on('open', function(id) {
-  //document.getElementById('log').innerHTML += 'client id: ' + id
-})
-
-peer.client.on('error', function(error) {
-  //document.getElementById('log').innerHTML += 'client error: ' + error
-})
-
-net.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
-net.send()
-
-function peerServerId() {
-  peer.server.id = JSON.parse(net.responseText).result[JSON.parse(net.responseText).result.length - 1].message.text
-  //document.getElementById('log').innerHTML += 'peer server id: ' + peer.server.id
-}
-
-peer.connectServer = setInterval(function() {
-  peer.connect = peer.client.connect(peer.server.id)
-
-  peer.connect.on('open', function() {
-    clearInterval(peer.connectServer)
-    peer.connect.send(peer.client.id)
-    //document.getElementById('log').innerHTML = 'open'
-  })
-
-  peer.connect.on('error', function(error) {
-    clearInterval(peer.connectServer)
-    //document.getElementById('log').innerHTML = 'connect error: ' + error
-  })
-}, 1000)
-
 // geolocation
 navigator.geolocation.getCurrentPosition(geolocation_position, geolocation_error)
 
@@ -68,28 +22,112 @@ document.addEventListener('touchstart', function(event) {
   //alert('geolocation x: ' + geolocation['x'] + '\n' + 'geolocatoion y: ' + geolocation['y'])
 }, false)
 
-// party!
-function partyOther () {
-  alert('other')
+// XMLHttpRequest
+var net = new XMLHttpRequest()
+
+net.onload = function() {
+  peerServerId()
 }
 
-function partyMy() {
-  alert('my')
+//peerJs
+var peer = []
+peer.client = new Peer()
+peer.client.id = []
+peer.server = []
+peer.server.id = []
+peer.connect = []
+peer.connect.server = []
+
+peer.client.on('open', function(id) {
+  //document.getElementById('log').innerHTML += 'peer client id: ' + peer.client.id
+})
+
+peer.client.on('error', function(error) {
+  //document.getElementById('log').innerHTML += 'peer client error: ' + error
+})
+
+net.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
+net.send()
+
+function peerServerId() {
+  peer.server.id = JSON.parse(net.responseText).result[JSON.parse(net.responseText).result.length - 1].message.text
+  //document.getElementById('log').innerHTML += 'peer server id: ' + peer.server.id
 }
 
+peer.connect.server = setInterval(function() {
+  peer.connect = peer.client.connect(peer.server.id)
+
+  peer.connect.on('open', function() {
+    clearInterval(peer.connectServer)
+    //document.getElementById('log').innerHTML = 'open'
+  })
+
+  peer.connect.on('error', function(error) {
+    clearInterval(peer.connectServer)
+    //document.getElementById('log').innerHTML = 'connect error: ' + error
+  })
+}, 1000)
+
+// party
 var party = []
+party.page = []
+party.page.connect = []
+party.page.intro = []
+party.page.intro.image = []
+party.page.intro.load = function() {
+  party.page.intro.image = document.createElement('IMG')
+  party.page.intro.image.src = 'intro.jpg'
+  document.body.append(party.page.intro.image)
+}
+party.page.other = []
+party.page.other.block = []
+party.page.other.other = []
+party.page.other.my = []
+party.page.other.number = []
+party.page.other.photo = []
+party.page.other.distance = []
+party.page.other.load = function() {
+  party.page.other.block = document.createElement('div')
+  party.page.other.block.className = 'block'
+  party.page.other.block.cssText =
+    ` 
+    width: 100 vw;
+    height: 100 vh;
+    background - color: lime;
+    display: grid;
+    grid - template - columns: 100 % ;
+    grid - template - rows: 90 % 10 % ;
+    grid - template - areas:
+    "party_other"
+    "party_my";
+    `
+  document.body.append(party.page.other.block)
 
+  party.page.other.other = document.createElement('div')
+  party.page.other.other.className = 'other'
+  party.page.other.other.cssText =
+    `
+    grid - area: party_other;
+    background - color: green;
+    overflow - y: scroll;
+    `
+  document.querySelector(party.page.other.block).append(party.page.other.other)
 
-party['other'] = []
-party['other']['number'] = []
-party['other']['photo'] = []
-party['other']['distance'] = []
-
-function partyOtherList () {
-  party['other']['number'][0] = document.createElement('div')
-  party['other']['number'][0].className = 'number_0'
-  party['other']['number'][0].style.cssText = 
-  ` width: 100vw;
+  party.page.other.my = document.createElement('div')
+  party.page.other.my.className = 'my'
+  party.page.other.my.cssText =
+    `
+    grid - area: party_my;
+    background - color: aqua;
+    `
+  document.querySelector(party.page.other.block).append(party.page.other.my)
+}
+party.page.other.list = function() {
+  party.page.other.number = document.createElement('div')
+  party.page.other.number.className = 'number_0'
+  party.page.other.number.style.cssText =
+    ` 
+    width: 100vw;
     height: 110vw;
     background-color: orange;
     display: grid;
@@ -97,27 +135,40 @@ function partyOtherList () {
     grid-template-rows: 90% 10%;
     grid-template-areas:
     "photo photo"
-    "people_number distance";`
-  
-  party['other']['photo'][0] = document.createElement('div')
-  party['other']['photo'][0].className = 'photo_0'
-  party['other']['photo'][0].style.cssText = 
-  ` grid-area: photo;
-    background-color: blue;`
-  
-  party['other']['distance'][0] = document.createElement('div')
-  party['other']['distance'][0].className = 'distance_0'
-  party['other']['distance'][0].style.cssText = 
-  ` grid-area: distance;
-    background-color: yellow;`
-  
-  document.querySelector('.party_other').append(party['other']['number'][0])
-  document.querySelector('.number_0').append(party['other']['photo'][0])
-  document.querySelector('.number_0').append(party['other']['distance'][0])
+    "people_number distance";
+    `
+  document.querySelector('.other').append(party.page.other.number)
+
+  party.page.other.photo = document.createElement('div')
+  party.page.other.photo.className = 'photo_0'
+  party.page.other.photo.style.cssText =
+    ` 
+    grid-area: photo;
+    background-color: blue;
+    `
+  document.querySelector('.number').append(party.page.other.photo)
+
+  party.page.other.distance = document.createElement('div')
+  party.page.other.distance.className = 'distance_0'
+  party.page.other.distance.style.cssText =
+    ` 
+    grid-area: distance;
+    background-color: yellow;
+    `
+
+  document.querySelector('.number').append(party.page.other.distance)
 }
+party.page.my = function() {
 
-partyOtherList()
-
-
-
-party['my'] = []
+}
+party.page.signal = function() {
+  
+}
+party.server = []
+party.server.connect = []
+party.server.id = function() {
+  peer.connect.send({ id: peer.client.id })
+}
+party.server.geolocation = function() {
+  peer.connect.send({ geolocation: { x: geolocation.x, y: geolocation.y } })
+}
