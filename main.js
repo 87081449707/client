7400a03ad8f246be169a97b0459fe54917f9144d
87081449707 <87081449707@mail.ru>
2020-08-17 03:54:31 +0300

    1

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ document.addEventListener('touchstart', function(event) {
   //alert('geolocation x: ' + geolocation['x'] + '\n' + 'geolocatoion y: ' + geolocation['y'])
 }, false)
 
-// party
+// party!
 function partyOther () {
   alert('other')
 }
@@ -75,13 +75,44 @@ function partyMy() {
   alert('my')
 }
 
-
 var party = []
-
 party['other'] = []
+party['other']['number'] = []
+party['other']['photo'] = []
+party['other']['distance'] = []
 
+function partyList () {
+  party['other']['number'][0] = document.createElement('div')
+  party['other']['number'][0].className = 'number_0'
+  party['other']['number'][0].style.cssText = 
+  ` width: 100vw;
+    height: 110vw;
+    background-color: orange;
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 90% 10%;
+    grid-template-areas:
+    "photo photo"
+    "people_number distance";`
+  
+  party['other']['photo'][0] = document.createElement('div')
+  party['other']['photo'][0].className = 'photo_0'
+  party['other']['photo'][0].style.cssText = 
+  ` grid-area: photo;
+    background-color: blue;`
+  
+  party['other']['distance'][0] = document.createElement('div')
+  party['other']['distance'][0].className = 'distance_0'
+  party['other']['distance'][0].style.cssText = 
+  ` grid-area: distance;
+    background-color: yellow;`
+  
+  document.querySelector('.party_other').append(party['other']['number'][0])
+  document.querySelector('.number_0').append(party['other']['photo'][0])
+  document.querySelector('.number_0').append(party['other']['distance'][0])
+}
 
-
+partyList()
 
 
 
