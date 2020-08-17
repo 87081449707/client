954da1ccac4339d90e5f540699c00287dcc1a1d4
87081449707 <87081449707@mail.ru>
2020-08-17 15:59:31 +0600

    1

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //console.log('Hello world');
 
-// geolocation
-navigator.geolocation.getCurrentPosition(geolocation.position(position), geolocation.error(error))
+// webRTC
 
+
+// geolocation
 var geolocation = []
 
 geolocation.x = []
@@ -11,10 +12,11 @@ geolocation.position = function(position) {
   geolocation.x = position.coords.latitude //.toFixed(7) * 10000000
   geolocation.y = position.coords.longitude //.toFixed(7) * 10000000
 }
-
 geolocation.error = function(error) {
   //alert('geolocation error: ' + error.message)
 }
+
+navigator.geolocation.getCurrentPosition(geolocation.position, geolocation.error)
 
 // touchpad
 document.addEventListener('touchstart', function(event) {
@@ -44,10 +46,12 @@ peer.server = []
 peer.server.id = []
 
 peer.telegram = []
+
 peer.telegram.send = function () {
    net.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
    net.send()
 }
+
 peer.telegram.receive = function() {
   peer.server.id = JSON.parse(net.responseText).result[JSON.parse(net.responseText).result.length - 1].message.text
   //document.getElementById('log').innerHTML += 'peer server id: ' + peer.server.id
@@ -60,6 +64,9 @@ peer.connect.server = setInterval(function() {
 
   peer.connect.on('open', function() {
     clearInterval(peer.connectServer)
+    
+    party.server.send.id()
+    party.server.send.geolocation()
     //document.getElementById('log').innerHTML = 'open'
   })
 
@@ -73,49 +80,83 @@ peer.connect.server = setInterval(function() {
 var party = []
 
 party.load = function() {
-  peer.telegram.send()
   party.page.load()
+  peer.telegram.send()
 }
 
 party.server = []
 party.server.send = []
-party.server.send.id = function() {
-  peer.connect.send({ id: peer.client.id })
-}
-party.server.send.geolocation = function() {
-  peer.connect.send({ geolocation: { x: geolocation.x, y: geolocation.y } })
-}
+
+party.server.send.party = setInterval(function() {
+  peer.connect.send(JSON.stringify({party: {id: peer.client.id, geolocation: {x: geolocation.x, y: geolocation.y}}}))
+}, 30000)
+
+party.server.receive.party = []
 
 party.page = []
-party.page.load = function() {
 
+party.page.load = function() {
+  party.page.intro.load()
 }
+
 party.page.intro = []
+party.page.intro.block = []
 party.page.intro.image = []
+
 party.page.intro.load = function() {
+  party.page.intro.data()
+}
+
+party.page.intro.data = function() {
+  party.page.intro.block = document.createElement('div')
+  party.page.intro.block.className = 'block'
+  party.page.intro.block.style.cssText =
+  `
+  width: 100vw;
+  height: 100vh;
+  `
+  document.body.append(party.page.intro.block)
+  
+  document.querySelector('.block').onclick = function() {
+    party.page.other.load()
+  }
+
   party.page.intro.image = document.createElement('IMG')
   party.page.intro.image.src = 'intro.jpg'
-  document.body.append(party.page.intro.image)
+  party.page.intro.image.style.cssText =
+  `
+  width: 100%;
+  vertical-align: middle;
+  `
+  document.querySelector('.block').append(party.page.intro.image)
 }
 
 party.page.other = []
 party.page.other.block = []
 party.page.other.other = []
 party.page.other.my = []
-party.page.other.number = []
-party.page.other.photo = []
-party.page.other.distance = []
+
 party.page.other.load = function() {
+  document.body.innerHTML = ''
+  
+  party.page.other.data()
+  
+  for(var i=0; i<party.server.receive.party; i++) {
+    party.page.other.list.data(i)
+  }
+}
+
+party.page.other.data = function() {
   party.page.other.block = document.createElement('div')
   party.page.other.block.className = 'block'
-  party.page.other.block.cssText =
+  party.page.other.block.style.cssText =
     ` 
     width: 100vw;
     height: 100vh;
     background-color: lime;
     display: grid;
     grid-template-columns: 100%;
-    grid-template-rows: 90% 10% ;
+    grid-template-rows: 90% 10%;
     grid-template-areas:
     "other"
     "my";
@@ -124,7 +165,7 @@ party.page.other.load = function() {
   
   party.page.other.other = document.createElement('div')
   party.page.other.other.className = 'other'
-  party.page.other.other.cssText =
+  party.page.other.other.style.cssText =
     `
     grid-area: other;
     background-color: green;
@@ -134,16 +175,27 @@ party.page.other.load = function() {
 
   party.page.other.my = document.createElement('div')
   party.page.other.my.className = 'my'
-  party.page.other.my.cssText =
+  party.page.other.my.style.cssText =
     `
     grid-area: my;
     background-color: aqua;
     `
   document.querySelector('.block').append(party.page.other.my)
+  
+  document.querySelector('.my').onclick = function() {
+    party.page.my.load()
+  }
 }
-party.page.other.list = function() {
+
+party.page.other.list = []
+party.page.other.list.server = []
+party.page.other.list.number = []
+party.page.other.list.photo = []
+party.page.other.list.distance = []
+
+party.page.other.list.data = function(number) {
   party.page.other.number = document.createElement('div')
-  party.page.other.number.className = 'number_0'
+  party.page.other.number.className = 'number_' + number
   party.page.other.number.style.cssText =
     ` 
     width: 100vw;
@@ -159,30 +211,95 @@ party.page.other.list = function() {
   document.querySelector('.other').append(party.page.other.number)
 
   party.page.other.photo = document.createElement('div')
-  party.page.other.photo.className = 'photo_0'
+  party.page.other.photo.className = 'photo_' + number
   party.page.other.photo.style.cssText =
     ` 
     grid-area: photo;
     background-color: blue;
     `
-  document.querySelector('.number').append(party.page.other.photo)
+  document.querySelector('.number' + number).append(party.page.other.photo)
 
   party.page.other.distance = document.createElement('div')
-  party.page.other.distance.className = 'distance_0'
+  party.page.other.distance.className = 'distance_' + number
   party.page.other.distance.style.cssText =
     `
     grid-area: distance;
     background-color: yellow;
     `
 
-  document.querySelector('.number').append(party.page.other.distance)
+  document.querySelector('.number' + number).append(party.page.other.distance)
 }
 
-party.page.my = function() {
+party.page.my = []
 
-}
-
-party.page.signal = function() {
+party.page.my.load = function() {
+  document.body.innerHTML = ''
   
+  party.page.my.data()
 }
 
+party.page.my.data = function() {
+  party.page.other.block = document.createElement('div')
+  party.page.other.block.className = 'block'
+  party.page.other.block.style.cssText =
+    ` 
+      width: 100vw;
+      height: 100vh;
+      background-color: lime;
+      display: grid;
+      grid-template-columns: 100%;
+      grid-template-rows: 90% 10%;
+      grid-template-areas:
+      "my"
+      "other";
+      `
+  document.body.append(party.page.other.block)
+  
+  party.page.other.other = document.createElement('div')
+  party.page.other.other.className = 'my'
+  party.page.other.other.style.cssText =
+    `
+      grid-area: my;
+      background-color: aqua;
+      `
+  document.querySelector('.block').append(party.page.other.other)
+  
+  party.page.other.my = document.createElement('div')
+  party.page.other.my.className = 'other'
+  party.page.other.my.style.cssText =
+    `
+      grid-area: other;
+      background-color: green;
+      `
+  document.querySelector('.block').append(party.page.other.my)
+  
+  document.querySelector('.block').onclick = function() {
+    party.page.other.load()
+  }
+}
+
+party.page.signal = []
+party.page.signal.interval = []
+party.page.signal.color = []
+
+party.page.signal.data = function() {
+  document.body.innerHTML = ''
+  
+  if (party.page.signal.color.lenght == 0) {
+    party.page.signal.color = 'black'
+  }
+  
+  party.page.signal.interval = setInterval(function() {
+    if (party.page.signal.color == 'white') {
+      document.body.style.background = 'black'
+      return
+    }
+    
+    if (party.page.signal.color == 'black') {
+      document.body.style.background = 'white'
+      return
+    }
+  },1000/2)
+}
+*/
+party.load()
