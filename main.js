16874ca432557d263e67e0f847bd821b8b581173
87081449707 <68937740+87081449707@users.noreply.github.com>
2020-08-20 17:38:34 +0600

    update main.js

--- main.js
+++ main.js
@@ -5,12 +5,11 @@ console.log('Hello world')
 // geolocation
 var geolocation_x
 var geolocation_y
-
-geolocation_position = function(position) {
+var geolocation_position = function(position) {
   geolocation_x = position.coords.latitude //.toFixed(7) * 10000000
   geolocation_y = position.coords.longitude //.toFixed(7) * 10000000
 }
-geolocation_error = function(error) {
+var geolocation_error = function(error) {
   //alert('geolocation error: ' + error.message)
 }
 
@@ -22,71 +21,77 @@ document.addEventListener('touchstart', function(event) {
   //console.log(server_id)
   //server.send('hi')
   //server_send()
-  server.send(JSON.stringify({ party: '', id: client_id, geolocation: { x: geolocation_x, y: geolocation_y }, number: 5 }))
+  server.send(JSON.stringify({
+    party: '', id: client_id, geolocation: {
+      x: geolocation_x, y: geolocation_y
+    }, number: 5
+  }))
 }, false)
 
 //peerJs
 var client
 var client_id
-
-client_connect = setInterval(function() {
+var client_connect = setInterval(function() {
   client = new Peer()
 
   client.on('open', function(id) {
     alert('peerJs client open')
-    
+
     client_id = id
 
     clearInterval(client_connect)
   })
-  
+
   client.on('error', function(error) {
     alert('peerJs client error: ' + error)
   })
-  
+
   client.on('connection', function(connect) {
     alert('prerJs server connect')
-  
+
     connect.on('data', function(data) {
       alert('peerJs client data') + data
-  
+
     })
   })
 }, 2000)
 
 var server
 var server_id
-
-server_connect = setInterval(function() {
+var server_connect = setInterval(function() {
   server = client.connect(server_id)
 
   server.on('open', function() {
     alert('peerJs server open')
-    
+
     clearInterval(server_connect)
-    
+
     server.send('hi')
   })
-  
+
   server.on('data', function(data) {
     alert('peerJs server data: ' + data)
-    
+
     server_receive(data)
   })
 
   server.on('error', function(error) {
     alert('peerJs server error: ' + error)
- 
-   clearInterval(server_connect)
+
+    clearInterval(server_connect)
   })
 }, 2000)
-
+var server_send = setInterval(function() {
+  //server.send(JSON.stringify({ party: '', id: client_id, geolocation: { x: geolocation_x, y: geolocation_y }, number: 5}))
+}, 5000)
+var server_receive = function(data) {
+  alert(data)
+}
 // telegram
 var telegram
+var telegram_send = setInterval(function() {
+  clearInterval(telegram_send)
 
-telegram_send = setInterval(function() {
- clearInterval(telegram_send)
- 
   // xmlHTTPrequest
   var xml = new XMLHttpRequest()
 
@@ -97,25 +102,19 @@ telegram_send = setInterval(function() {
   xml.open('GET', 'https://api.telegram.org/bot1281235712:AAH8j6p2BIW2BDd3wPPZdoD3abIAyyoB4Yk/getUpdates', false)
   xml.send()
 }, 1000)
-telegram_receive = function(data) {
+var telegram_receive = function(data) {
   alert('peerJs server id: ' + JSON.parse(data).result[JSON.parse(data).result.length - 1].message.text)
-  
+
   server_id = JSON.parse(data).result[JSON.parse(data).result.length - 1].message.text
-}
-server_send = setInterval(function() {
-  //server.send(JSON.stringify({ party: '', id: client_id, geolocation: { x: geolocation_x, y: geolocation_y }, number: 5}))
-}, 5000)
-server_receive = function(data) {
-  alert(data)
 }
 
 // page
-page_number = function (){
+var page_number = function () {
   
 }
-page_intro = setInterval(function() {
+var page_intro = setInterval(function() {
   clearInterval(page_intro)
-  
+
   document.body.innerHTML = ''
 
   var block
@@ -123,11 +122,11 @@ page_intro = setInterval(function() {
   block = document.createElement('div')
   block.className = 'block'
   block.style.cssText =
-    `
+  `
   width: 100vw;
   height: 100vh;
   `
-
+  
   document.body.append(block)
 
   document.querySelector('.block').onclick = function() {
@@ -139,14 +138,14 @@ page_intro = setInterval(function() {
   image = document.createElement('IMG')
   image.src = 'intro.jpg'
   image.style.cssText =
-    `
+  `
   width: 100%;
   vertical-align: middle;
   `
 
   document.querySelector('.block').append(image)
 }, 1000)
-page_other = function() {
+var page_other = function() {
   document.body.innerHTML = ''
 
   var block
@@ -154,17 +153,17 @@ page_other = function() {
   block = document.createElement('div')
   block.className = 'block'
   block.style.cssText =
-    ` 
-    width: 100vw;
-    height: 100vh;
-    background-color: lime;
-    display: grid;
-    grid-template-columns: 100%;
-    grid-template-rows: 90% 10%;
-    grid-template-areas:
-    "other"
-    "my";
-    `
+  `
+  width: 100vw;
+  height: 100vh;
+  background-color: lime;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 90% 10%;
+  grid-template-areas:
+  "other"
+  "my";
+  `
 
   document.body.append(block)
 
@@ -173,11 +172,11 @@ page_other = function() {
   other = document.createElement('div')
   other.className = 'other'
   other.style.cssText =
-    `
-    grid-area: other;
-    background-color: green;
-    overflow-y: scroll;
-    `
+  `
+  grid-area: other;
+  background-color: green;
+  overflow-y: scroll;
+  `
 
   document.querySelector('.block').append(other)
 
@@ -186,10 +185,10 @@ page_other = function() {
   my = document.createElement('div')
   my.className = 'my'
   my.style.cssText =
-    `
-    grid-area: my;
-    background-color: aqua;
-    `
+  `
+  grid-area: my;
+  background-color: aqua;
+  `
 
   document.querySelector('.block').append(my)
 
@@ -197,22 +196,24 @@ page_other = function() {
     page_my()
   }
 }
-page_other_list = function() {
+var page_other_list = function() {
+  //document.body.innerHTML = ''
+  
   var block
 
-  block_list = document.createElement('div')
-  block_list.className = 'block'
-  block_list.style.cssText =
-    ` 
-    width: 100vw;
-    height: 50vw;
-    background-color: orange;
-    display: grid;
-    grid-template-columns: 50% 50%;
-    grid-template-rows: 100%;
-    grid-template-areas:
-    "distance people";
-    `
+  block = document.createElement('div')
+  block.className = 'block'
+  block.style.cssText =
+  `
+  width: 100vw;
+  height: 50vw;
+  background-color: orange;
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: 100%;
+  grid-template-areas:
+  "distance people";
+  `
 
   document.querySelector('.other').append(block)
 
@@ -221,10 +222,10 @@ page_other_list = function() {
   distance = document.createElement('div')
   distance.className = 'distance'
   distance.style.cssText =
-    `
-    grid-area: distance;
-    background-color: yellow;
-    `
+  `
+  grid-area: distance;
+  background-color: yellow;
+  `
 
   document.querySelector('.block').append(distance)
 
@@ -233,15 +234,15 @@ page_other_list = function() {
   photo = document.createElement('div')
   photo.className = 'people'
   photo.style.cssText =
-    ` 
-    grid-area: people;
-    background-color: blue;
-    `
+  `
+  grid-area: people;
+  background-color: blue;
+  `
 
   document.querySelector('.block').append(people)
 
 }
-page_my = function() {
+var page_my = function() {
   document.body.innerHTML = ''
 
   var block
@@ -249,17 +250,17 @@ page_my = function() {
   block = document.createElement('div')
   block.className = 'block'
   block.style.cssText =
-    ` 
-      width: 100vw;
-      height: 100vh;
-      background-color: lime;
-      display: grid;
-      grid-template-columns: 100%;
-      grid-template-rows: 90% 10%;
-      grid-template-areas:
-      "my"
-      "other";
-      `
+  `
+  width: 100vw;
+  height: 100vh;
+  background-color: lime;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-template-rows: 90% 10%;
+  grid-template-areas:
+  "my"
+  "other";
+  `
 
   document.body.append(block)
 
@@ -268,10 +269,10 @@ page_my = function() {
   my = document.createElement('div')
   my.className = 'my'
   my.style.cssText =
-    `
-    grid-area: my;
-    background-color: green;
-    `
+  `
+  grid-area: my;
+  background-color: green;
+  `
 
   document.querySelector('.block').append(my)
 
@@ -280,10 +281,10 @@ page_my = function() {
   other = document.createElement('div')
   other.className = 'other'
   other.style.cssText =
-    `
-    grid-area: other;
-    background-color: aqua;
-    `
+  `
+  grid-area: other;
+  background-color: aqua;
+  `
 
   document.querySelector('.block').append(other)
 
@@ -291,7 +292,7 @@ page_my = function() {
     page_other()
   }
 }
-page_signal = function() {
+var page_signal = function() {
   document.body.innerHTML = ''
 
   var color = 'black'
@@ -306,6 +307,6 @@ page_signal = function() {
       document.body.style.background = 'white'
       return
     }
-  }, 1000 / 3)
+  },
+    1000 / 3)
 }
-
